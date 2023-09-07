fb9a957d93999738af9b9d3906d80a690dc7bf43
Onyekachi Acha <achawaskey@gmail.com>
2023-09-07 16:54:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that print a line with puts function
+ * Return: o (Success)
+ */
+int main(vold)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (o);
+}
