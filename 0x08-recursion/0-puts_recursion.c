bf731e5b3abb15a0cdbc6f1381f14a3c541ac932
Onyekachi Acha <achawaskey@gmail.com>
2023-09-30 17:15:01 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,9 @@
 * @s: input
 * Return: Always 0 (Success)
 */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
