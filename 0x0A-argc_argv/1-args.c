844612649836cfe546bc3cb30527985b1e4f8860
Onyekachi Acha <achawaskey@gmail.com>
2023-10-02 18:42:32 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv; / "ignore argv" /
+	(void) argv; /*ignore argv*/
 		printf("%d\n", argc - 1);
 	return (0);
 }
