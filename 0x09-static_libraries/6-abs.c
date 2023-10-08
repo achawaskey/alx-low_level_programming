4c5f29ec28f738d594c160b48478ca651a06d674
Onyekachi Acha <achawaskey@gmail.com>
2023-10-07 19:09:14 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - computers the absolute value of an integer
+* @n: the int to check
+* REturn: the absolute value of int
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
