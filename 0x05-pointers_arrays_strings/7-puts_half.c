4710110308ce1868d4b734d68cfeeb22bd26eba4
Onyekachi Acha <achawaskey@gmail.com>
2023-10-21 19:52:41 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,10 +14,10 @@ void puts_half(char *str)
 
 	for (a = 0; str[a] != '\0'; a++)
 		longi++;
-	n = ((longi % 2) == 1)
+	n = ((longi % 2) == 1);
 		n = ((longi + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
-		_putchar(str[a];
+		_putchar(str[a]);
 	_putchar('\n');
 }
