76e7445e803d772653699fb2e93a7b4edabe0b3d
Onyekachi Acha <achawaskey@gmail.com>
2023-10-22 06:00:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,45 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* _strlen - find lenght of a string
-* @s: string
-* Return: int
-*/
-
-int _strlen(char *s)
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
 {
-	int size = 0;
+	char *conct;
+	int i, ci;
 
-	for (; s[size] != '\0'; size++)
-		;
-	return (size);
-}
-
-/**
-* _str_concat - get ends of input and add together for size
-* @s1: input one to concat
-* @s2: input two to concat
-* Return: concat of s1 and s2
-*/
-char *str_concat(char *s1, char *s2);
-{
-	int size1, size2, i;
-	char *m;
-
-	if (s1 == NULL)
-		s1 = "\0";
 	if (s2 == NULL)
-		s2 = "\0";
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	m = malloc((size1 + size2) * sizeof(char) + 1);
-	if (m == 0)
-		return (0);
+	i = ci = 0;
 
-	for (i = 0; i <= size1 + size2; i++)
+	while (s2[ci] != '\0')
+		i++;
+
+	conct = malloc(sizeof(char) * (i + ci + 1));
+	if (conct == NULL)
+		return (NULL);
+
+	i = ci = 0;
+
+	while (s1[i] != '\0')
 	{
-		if (i < size1)
-			m[i] = s1[i];
-		else
-			m[i] = s2[i - size1];
+		conct[i] = s1[i];
+		i++;
 	}
-	m[i] = '\0';
-	return (m);
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++;
+	}
+
+	conct[i] = '\0';
+	return (conct);
 }
