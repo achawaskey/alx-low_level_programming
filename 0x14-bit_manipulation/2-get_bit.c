5b4db5de87d8b8de3f61b00ae84de80a0b814390
Onyekachi <achawaskey@gmail.com>
2023-10-28 04:15:18 +0100

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * get_bit - prog returns the of a bit at a given index
- * @n: number to check bits in
- * @index index at which to check bit
- *
- * Return: value of the bit, or -1 if there is an error
- */
+* get_bit - prog returns the of a bit at a given index
+* @n: number to check bits in
+* @index: index at which to check bit
+*
+* Return: value of the bit, or -1 if there is an error
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	unsigned int i;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > sizeof(size_t) * 8)
 		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+
+	for (i = 0; i < index; i++)
+		n = n >> 1;
+	return ((n & 1));
 }
