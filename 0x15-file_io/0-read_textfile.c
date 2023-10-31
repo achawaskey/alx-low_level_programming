6c791f6498af9bd50d41d8a1698e87ad11e51a7f
Onyekachi <achawaskey@gmail.com>
2023-10-31 01:16:28 +0100

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - This reads a text file and prints it to the POSTX stdo
+ *
+ * @filename: name of the file to read
+ *
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t nrd, nwr;
+	char *buff;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
+
+	close(fd);
+	free(buff);
+
+	return (nwr);
+}
