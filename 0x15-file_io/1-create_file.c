c11f16e6464eccb7ae3bf2ea7c82bcde2e046453
Onyekachi <achawaskey@gmail.com>
2023-11-02 23:17:07 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,28 +8,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int file_d;
 	int nletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (file_d == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
 
-	rwr = write(fd, text_content, nletters);
+	rwr = write(file_d, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
-	close(fd);
+	close(file_d);
 
 	return (1);
 }
